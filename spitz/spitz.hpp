7b6055876e311a0015fc50738565b074fe61f1b1
Ian Liu Rodrigues <ian.liu88@gmail.com>
2014-07-28 14:19:39 -0300

    Fix C++ API

--- spitz/spitz.hpp
+++ spitz/spitz.hpp
@@ -1,26 +1,83 @@
 #ifndef __SPITZ_CPP__
 #define __SPITZ_CPP__
 
+#include <spitz/stream.hpp>
+
 namespace spitz {
-  class JobManager {
+  class jobmanager {
     public:
-      virtual void setup(int argc, char *argv[]) {};
-      virtual bool next_task(Stream& stream) = 0;
+      virtual bool next_task(stream& stream) = 0;
   };
 
-  class Worker {
+  class worker {
     public:
-      virtual void setup(int argc, char *argv[]) {};
-      virtual void run(const Stream& task, Stream& result) = 0;
+      virtual void run(stream& task, stream& result) = 0;
   };
 
-  class Committer {
+  class committer {
     public:
-      virtual void setup(int argc, char *argv[]) {};
-      virtual void commit_task(const Stream& result) = 0;
-      virtual void commit_job(Stream& final_result) {};
+      virtual void commit_task(stream& result) = 0;
+      virtual void commit_job(stream& final_result) {
+        (void)(final_result);
+      }
+  };
+
+  class factory {
+    public:
+      virtual jobmanager *create_jobmanager(int, char **) = 0;
+      virtual worker *create_worker(int, char **) = 0;
+      virtual committer *create_committer(int, char **) = 0;
   };
 };
+
+extern spitz::factory *spitz_factory;
+
+extern "C" void *spits_job_manager_new(int argc, char *argv[])
+{
+  spitz::jobmanager *jm = spitz_factory->create_jobmanager(argc, argv);
+  return reinterpret_cast<void*>(jm);
+}
+
+extern "C" int spits_job_manager_next_task(void *user_data, struct byte_array *ba)
+{
+  class spitz::jobmanager *jm = reinterpret_cast<spitz::jobmanager*>(user_data);
+  spitz::stream task(ba);
+  return jm->next_task(task) ? 1 : 0;
+}
+
+extern "C" void *spits_worker_new(int argc, char **argv)
+{
+  spitz::worker *w = spitz_factory->create_worker(argc, argv);
+  return reinterpret_cast<void*>(w);
+}
+
+extern "C" void spits_worker_run(void *user_data, struct byte_array *task, struct byte_array *result)
+{
+  spitz::worker *w = reinterpret_cast<spitz::worker*>(user_data);
+  spitz::stream _task(task);
+  spitz::stream _result(result);
+  w->run(_task, _result);
+}
+
+extern "C" void *spits_setup_commit(int argc, char *argv[])
+{
+  spitz::committer *co = spitz_factory->create_committer(argc, argv);
+  return reinterpret_cast<void*>(co);
+}
+
+extern "C" void spits_commit_pit(void *user_data, struct byte_array *result)
+{
+  spitz::committer *co = reinterpret_cast<spitz::committer*>(user_data);
+  spitz::stream _result(result);
+  co->commit_task(_result);
+}
+
+extern "C" void spits_commit_job(void *user_data, struct byte_array *final_result)
+{
+  spitz::committer *co = reinterpret_cast<spitz::committer*>(user_data);
+  spitz::stream _final_result(final_result);
+  co->commit_job(_final_result);
+}
 
 #endif /* __SPITZ_CPP__ */
 
--- spitz/stream.hpp
+++ spitz/stream.hpp
@@ -4,35 +4,33 @@
 #include <spitz/barray.h>
 
 namespace spitz {
-  class Stream {
+  class stream {
     private:
-      struct byte_array ba;
+      struct byte_array *ba;
 
     public:
-      Stream(size_t cap) {
-        byte_array_init(&ba, cap);
-      }
+      stream(struct byte_array *ba) : ba(ba) { }
 
-      stream& operator<<(const uint8_t& v)  { _byte_array_pack8(&ba, v); return *this; }
-      stream& operator>>(      uint8_t& v)  { _byte_array_unpack8(&ba, &v); return *this; }
+      stream& operator<<(const uint8_t& v)  { _byte_array_pack8(ba, v); return *this; }
+      stream& operator>>(      uint8_t& v)  { _byte_array_unpack8(ba, &v); return *this; }
 
-      stream& operator<<(const uint16_t& v) { _byte_array_pack16(&ba, v); return *this; }
-      stream& operator>>(      uint16_t& v) { _byte_array_unpack16(&ba, &v); return *this; }
+      stream& operator<<(const uint16_t& v) { _byte_array_pack16(ba, v); return *this; }
+      stream& operator>>(      uint16_t& v) { _byte_array_unpack16(ba, &v); return *this; }
 
-      stream& operator<<(const uint32_t& v) { _byte_array_pack32(&ba, v); return *this; }
-      stream& operator>>(      uint32_t& v) { _byte_array_unpack32(&ba, &v); return *this; }
+      stream& operator<<(const uint32_t& v) { _byte_array_pack32(ba, v); return *this; }
+      stream& operator>>(      uint32_t& v) { _byte_array_unpack32(ba, &v); return *this; }
 
-      stream& operator<<(const uint64_t& v) { _byte_array_pack64(&ba, v); return *this; }
-      stream& operator>>(      uint64_t& v) { _byte_array_unpack64(&ba, &v); return *this; }
+      stream& operator<<(const uint64_t& v) { _byte_array_pack64(ba, v); return *this; }
+      stream& operator>>(      uint64_t& v) { _byte_array_unpack64(ba, &v); return *this; }
 
-      stream& operator<<(const int& v)      { byte_array_pack32(&ba, v); return *this; }
-      stream& operator>>(      int& v)      { byte_array_unpack32(&ba, &v); return *this; }
+      stream& operator<<(const int& v)      { byte_array_pack32(ba, v); return *this; }
+      stream& operator>>(      int& v)      { byte_array_unpack32(ba, &v); return *this; }
 
-      stream& operator<<(const float& v)    { byte_array_pack32(&ba, v); return *this; }
-      stream& operator>>(      float& v)    { byte_array_unpack32(&ba, &v); return *this; }
+      stream& operator<<(const float& v)    { byte_array_pack32(ba, v); return *this; }
+      stream& operator>>(      float& v)    { byte_array_unpack32(ba, &v); return *this; }
 
-      stream& operator<<(const double& v)   { byte_array_pack64(&ba, v); return *this; }
-      stream& operator>>(      double& v)   { byte_array_unpack64(&ba, &v); return *this; }
+      stream& operator<<(const double& v)   { byte_array_pack64(ba, v); return *this; }
+      stream& operator>>(      double& v)   { byte_array_unpack64(ba, &v); return *this; }
   };
 };
 
