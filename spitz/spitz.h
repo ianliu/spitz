b3b43bc2521cbb309ad5ae43882601a3cec30873
Ian Liu Rodrigues <ian.liu88@gmail.com>
2014-08-13 13:35:28 -0300

    Add C++ extern "C" in spitz.h header

--- spitz/spitz.h
+++ spitz/spitz.h
@@ -1,6 +1,10 @@
 #ifndef __SPITZ_H__
 #define __SPITZ_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /*
  * Returns the worker id. If this method is called in a non-worker entity, it
  * returns -1.
@@ -11,5 +15,9 @@ int spitz_get_worker_id(void);
  * Returns the number of workers in this process.
  */
 int spitz_get_num_workers(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __SPITZ_H__ */
